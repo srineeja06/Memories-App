33183ae3764cabbf270941dc0392cacae829b662
srineeja@06 <jangamsrineeja@gmail.com>
2026-07-24 20:35:30 +0530

    add react redux toolkit

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react'
+import React, { use, useState } from 'react'
 import PasswordInput from '../../components/PasswordInput'
 import {useNavigate} from "react-router-dom"
 import { validateEmail } from '../../utils/helper.js'
 import axiosInstance from '../../utils/axiosInstance'
+import { useDispatch, useSelector } from 'react-redux'
+import { signInStart, signInSuccess } from '../../redux/slice/userSlice.js'
 
 const Login = () => {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
   const [email, setEmail] = useState("")
   const [password,setPassword] =useState("")
   const [error,setError] = useState("")
+
+  const { loading } = useSelector((state) => state.user)
 
   const handleSubmit = async(e) => {
     e.preventDefault()
@@ -26,12 +31,15 @@ const Login = () => {
     setError(null)
 
     try {
+      dispatch(signInStart())
+
       const response = await axiosInstance.post("/auth/signin", {
         email,
         password,
       })
 
       if (response.data) {
+        dispatch(signInSuccess(response.data))
         navigate("/")
       }
     } catch (error) {
@@ -85,9 +93,16 @@ const Login = () => {
 
               {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
 
-            <button type='submit' className='btn-primary'>
-              LOGIN
-            </button>
+            {loading ? 
+              (
+                <span className='animate-pulse'>Loading.....</span>
+              ) : 
+              (
+                <button type='submit' className='btn-primary'>
+                  LOGIN
+                </button>
+              )
+            }
 
             <p className='text-xs text-slate-500 text-center my-4'> Or</p>
 
