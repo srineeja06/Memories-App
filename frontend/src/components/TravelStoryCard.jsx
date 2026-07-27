1aa86e2a2a9277d71628c73603b08ee9726b5a16
srineeja@06 <jangamsrineeja@gmail.com>
2026-07-27 13:51:02 +0530

    travel card component

---
+++ frontend/src/components/TravelStoryCard.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import moment from "moment"
+import { VscLocation, VscWhitespace } from "react-icons/vsc";
+import { PiHeartStraightFill } from "react-icons/pi";
+
+const TravelStoryCard = ({ 
+    imageUrl, 
+    title, 
+    story, 
+    date,
+    location, 
+    isFavorite, 
+    onEdit, 
+    onClick, 
+    onFavoriteClick 
+}) => {
+  return (
+    <div className='border border-slate-200 rounded-lg overflow-hidden bg-white hover:shadow-lg hover:shaadow-slate-200 transition-all ease-in-out relative cursor-pointer'>
+        <img 
+            src={imageUrl} 
+            alt={title} 
+            className='w-full h-56 object-cover rounded-lg'
+            onClick={onClick}
+        />
+
+        <button 
+            className='w-12 h-12 flex items-center justify-center bg-white/40 rounded-lg border border-white/30 absolute top-4 right-4' 
+            onClick={onFavoriteClick}
+        >
+            <PiHeartStraightFill className={ `icon-btn ${isFavorite ? "text-black" : "text-white"} hover:text-black` }/> 
+        </button>
+
+        <div className='p-4' onClick={onClick}>
+            <div className='flex items-center gap-3'>
+                <div className='flex-1'>
+                    <h6 className='text-[16px] font-medium'>{title}</h6>
+
+                    <span className='text-xs text-slate-500'>
+                        {date ? moment(date).format("MMM Do YYYY") : "-"}
+                    </span>
+                </div>
+            </div>
+
+            <p className='text-sm text-slate-600 mt-2'>{story ?.slice(0,50)}</p>
+
+            <div className='inline-flex items-center gap-2 text-[13px] text-cyan-600 bg-cyan-200/40 rounded mt-2 px-2 py-1'>
+             <VscLocation className='text-sm'/>
+             { location.map((item, index) => location.length === index + 1 ? `${item}` : `${item}, ` )}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TravelStoryCard
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar'
 import axiosInstance from '../../utils/axiosInstance'
+import TravelStoryCard from '../../components/TravelStoryCard'
+import { data } from 'react-router-dom'
 
 const Home = () => {
   const [ allStories, setAllStories] = useState([])
 
   console.log(allStories)
-  
+
   //all stories
   const getAllTravelStories = async () => {
     try {
@@ -20,6 +22,13 @@ const Home = () => {
     }
   }
 
+  //handle edit
+  const handleEdit = async (data) => {}
+
+  const handleViewStory = (data) => {}
+
+  const updateIsFavorite = async (data) => {}
+
   useEffect(() => {
     getAllTravelStories()
 
@@ -32,7 +41,30 @@ const Home = () => {
 
       <div className='container mx-auto py-10'>
         <div className='flex gap-7'>
-          <div className='flex-1'></div>
+          <div className='flex-1'>
+            {allStories.length > 0 ? (
+              <div className='grid grid-cols-2 gap-4'>
+                {allStories.map((item) => {
+                  return (
+                    <TravelStoryCard 
+                      key={item._id} 
+                      imageUrl={item.imageUrl} 
+                      title={item.title} 
+                      story={item.story} 
+                      date={item.visitedDate}
+                      location={item.visitedLocation} 
+                      isFavorite = {item.isFavorite} 
+                      onEdit={() => handleEdit(item)}
+                      onClick={()=>handleViewStory(item)}
+                      onFavoriteClick={()=>updateIsFavorite(item)}
+                    />
+                  )
+                })}
+              </div>
+            ) : (
+              <div>Empty Card Here</div>
+            )}
+          </div>
 
           <div className='w-[320px]'></div>
         </div>
